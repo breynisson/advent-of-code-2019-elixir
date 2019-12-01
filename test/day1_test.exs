dc8c2d32357edbef24774e3274e80663fdd288e5
breynisson <bjorgvin.reynisson@gmail.com>
2019-12-01 23:09:17 +0000

    Add second part of Day1.

--- test/day1_test.exs
+++ test/day1_test.exs
@@ -2,11 +2,19 @@ defmodule AdventElixirTest do
   use ExUnit.Case
   doctest Day1
 
-  test "mass 12 should yield fuel 2" do
-    assert Day1.calculate_fuel_from_mass(12) == 2
+  test "mass 12 should yield 2 fuel" do
+    assert Day1.calculate_total_fuel_from_mass(12) == 2
   end
 
-  test "mass 14 should yield fuel 2" do
-    assert Day1.calculate_fuel_from_mass(14) == 2
+  test "mass 14 should yield 2 fuel" do
+    assert Day1.calculate_total_fuel_from_mass(14) == 2
+  end
+
+  test "mass 1969 should yield 966 fuel" do
+    assert Day1.calculate_total_fuel_from_mass(1969) == 966
+  end
+
+  test "mass 100756 should yield 50346 fuel" do
+    assert Day1.calculate_total_fuel_from_mass(100756) == 50346
   end
 end
